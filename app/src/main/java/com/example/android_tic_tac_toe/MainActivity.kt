07f0000621c44df5a631f81567649910b2d4bbab
RonyL02 <280202rony@gmail.com>
2024-12-05 20:22:48 +0200

    add onclick listener

--- app/src/main/java/com/example/android_tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/android_tic_tac_toe/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.android_tic_tac_toe
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.android_tic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initCells()
+    }
+
+    private fun initCells() {
+        val grid = binding.grid
+
+        for (i in 0..<grid.childCount) {
+            val cell = grid.getChildAt(i)
+
+            cell.setOnClickListener { view ->
+                (view as ImageView).setImageResource(R.drawable.circle)
+            }
         }
     }
 }
