31006c8bca3ecebd23ac38f06df189a8f87c9d4d
Shiran1212 <shiran.shmu@gmail.com>
2024-12-05 20:16:06 +0200

    changes

--- app/src/main/java/com/example/android_tic_tac_toe/Game.kt
+++ app/src/main/java/com/example/android_tic_tac_toe/Game.kt
@@ -1,16 +1,21 @@
 package com.example.android_tic_tac_toe
 
 class Game {
+    enum class CellState {
+        X,
+        O,
+        EMPTY
+    }
 
-    private val board: Array<Array<String>> = Array(3) { Array(3) { " " } }
-    var currentPlayer: String = "X"
-        private set
+    private val board: Array<Array<CellState>> = Array(3) { Array(3) { CellState.EMPTY} }
+    private var currentPlayer: CellState=CellState.X
+
 
     fun makeMove(row: Int, col: Int): Boolean {
         if (row !in 0..2 || col !in 0..2) {
             return false
         }
-        if (board[row][col] != " ") {
+        if (board[row][col] != CellState.EMPTY) {
             return false
         }
 
@@ -20,26 +25,35 @@ class Game {
 
 
     fun switchPlayer() {
-        currentPlayer = if (currentPlayer == "X") "O" else "X"
+        currentPlayer = if (currentPlayer == CellState.X) CellState.O else CellState.X
     }
 
-    fun checkWinner(): String? {
+    fun checkWinner(): CellState? {
 
         for (i in 0..2) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != " ") {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != CellState.EMPTY) {
                 return board[i][0]
             }
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != " ") {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != CellState.EMPTY) {
                 return board[0][i]
             }
         }
 
+
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != CellState.EMPTY) {
+            return board[0][0]
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != CellState.EMPTY) {
+            return board[0][2]
+        }
         return null
     }
 
+
+
     fun isBoardFull(): Boolean {
         for (row in board) {
-            if (row.contains(" ")) {
+            if (row.contains(CellState.EMPTY)) {
                 return false
             }
         }
@@ -49,13 +63,13 @@ class Game {
     fun resetGame() {
         for (i in 0..2) {
             for (j in 0..2) {
-                board[i][j] = " "
+                board[i][j] = CellState.EMPTY
             }
         }
-        currentPlayer = "X"
+        currentPlayer = CellState.X
     }
 
-    fun getBoard(): Array<Array<String>> {
+    fun getBoard(): Array<Array<CellState>> {
         return board
     }
 
