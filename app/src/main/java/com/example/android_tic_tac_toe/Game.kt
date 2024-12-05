d4097969db97570df23647e040a597dcd6c19001
RonyL02 <280202rony@gmail.com>
2024-12-05 23:41:20 +0200

    bind game to ui

--- app/src/main/java/com/example/android_tic_tac_toe/Game.kt
+++ app/src/main/java/com/example/android_tic_tac_toe/Game.kt
@@ -1,54 +1,57 @@
 package com.example.android_tic_tac_toe
 
 class Game {
-    enum class CellState {
-        X,
-        O,
-        EMPTY
+    companion object {
+        val GRID_SIZE = 3
     }
 
-    private val board: Array<Array<CellState>> = Array(3) { Array(3) { CellState.EMPTY} }
-    private var currentPlayer: CellState=CellState.X
+    enum class CellState(val value: String) {
+        X("X"),
+        O("O"),
+        EMPTY("EMPTY")
+    }
 
+    private val board: Array<Array<CellState>> =
+        Array(GRID_SIZE) { Array(GRID_SIZE) { CellState.EMPTY } }
+    var currentPlayer: CellState = CellState.X
+        private set
 
     fun makeMove(row: Int, col: Int): Boolean {
-        if (row !in 0..2 || col !in 0..2) {
+        if (row !in board.indices || col !in board.indices) {
             return false
         }
+
         if (board[row][col] != CellState.EMPTY) {
             return false
         }
 
         board[row][col] = currentPlayer
+
         return true
     }
-
 
     fun switchPlayer() {
         currentPlayer = if (currentPlayer == CellState.X) CellState.O else CellState.X
     }
 
-    fun checkWinner(): CellState? {
-
-        for (i in 0..2) {
+    fun checkWinner(): CellState {
+        for (i in board.indices) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != CellState.EMPTY) {
                 return board[i][0]
             }
             if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != CellState.EMPTY) {
                 return board[0][i]
             }
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != CellState.EMPTY) {
+                return board[0][0]
+            }
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != CellState.EMPTY) {
+                return board[0][2]
+            }
         }
 
-
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != CellState.EMPTY) {
-            return board[0][0]
-        }
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != CellState.EMPTY) {
-            return board[0][2]
-        }
-        return null
+        return CellState.EMPTY
     }
-
 
 
     fun isBoardFull(): Boolean {
@@ -57,6 +60,7 @@ class Game {
                 return false
             }
         }
+
         return true
     }
 
@@ -68,10 +72,4 @@ class Game {
         }
         currentPlayer = CellState.X
     }
-
-    fun getBoard(): Array<Array<CellState>> {
-        return board
-    }
-
-
 }
--- app/src/main/java/com/example/android_tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/android_tic_tac_toe/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.android_tic_tac_toe
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,6 +12,7 @@ import com.example.android_tic_tac_toe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var game: Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +25,66 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        game = Game()
         initCells()
+        updateTurnButton()
+    }
+
+    private fun updateTurnButton() {
+        binding.turnButton.text =
+            resources.getString(R.string.player_turn, game.currentPlayer.value)
     }
 
     private fun initCells() {
         val grid = binding.grid
 
         for (i in 0..<grid.childCount) {
-            val cell = grid.getChildAt(i)
+            val cell = grid.getChildAt(i) as ImageView
+            cell.setImageDrawable(null)
 
-            cell.setOnClickListener { view ->
-                (view as ImageView).setImageResource(R.drawable.circle)
-            }
+            cell.id = i
+            cell.setOnClickListener(::onclick)
         }
+    }
+
+    private fun onclick(view: View) {
+        val row = view.id / 3
+        val col = view.id % 3
+
+        val isMoveValid = game.makeMove(row, col)
+
+        if (!isMoveValid) return
+
+        val imageId =
+            if (game.currentPlayer == Game.CellState.X) R.drawable.x else R.drawable.circle
+
+        (view as ImageView).setImageResource(imageId)
+
+        val winner = game.checkWinner()
+
+        if (winner != Game.CellState.EMPTY) {
+            openFinishDialog("Player ${game.currentPlayer.value} won")
+        } else if (game.isBoardFull()) {
+            openFinishDialog("It's a draw")
+        } else {
+            game.switchPlayer()
+            updateTurnButton()
+        }
+    }
+
+    private fun openFinishDialog(title: String) {
+        AlertDialog.Builder(this)
+            .setMessage(title)
+            .setPositiveButton("play again") { _, _ ->
+                game.resetGame()
+                initCells()
+            }
+            .setNegativeButton("exit") { _, _ ->
+                finish()
+            }
+            .setOnCancelListener {
+                finish()
+            }
+            .show()
     }
 }
